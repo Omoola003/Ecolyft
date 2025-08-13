be6ebfea7348223771db9aa133bd4082cf64e98b
Omoola003 <owolabigbolahan003@gmail.com>
2025-08-13 17:08:07 +0100

    Initial commit from Create Next App

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+theme: {
+  extend: {
+    colors: {
+      cleanBlue: "#4A90E2",
+      ecoGreen: "#2E8B57",
+      refinedGold: "#FFD700",
+      earthBrown: "#7F5C4F",
+      boldBlack: "#000000",
+      softGray: "#F4F4F4",
+    },
+  },
+}
